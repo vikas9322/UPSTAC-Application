674bf9fbde3ceb14f1b06303f696bd7fe65be507
vikas <panditvikas18@gmail.com>
2022-02-09 14:06:31 +0530

    this is a source file commit

---
+++ src/main/java/org/upgrad/upstac/config/security/UserLoggedInService.java
@@ -0,0 +1,29 @@
+package org.upgrad.upstac.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.upgrad.upstac.users.User;
+import org.upgrad.upstac.users.UserService;
+
+
+@Component
+public class UserLoggedInService {
+
+    private UserService userService;
+
+    @Autowired
+    public UserLoggedInService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    public User getLoggedInUser() {
+            UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return userService.findByUserName();
+
+    }
+
+
+}
